feb4e7f2de8492664af783bd2c60813d5f82648d
misgo78 <anthonygogomis@gmail.com>
2024-10-09 17:43:33 +0200

    Nettoyage du df

--- app/script.py
+++ app/script.py
@@ -6,10 +6,29 @@ spark = SparkSession.builder \
 	.getOrCreate()
 
 # Lecture du fichier csv
-df = spark.read.text("hdfs://namenode:9000/Datasets/chat_messages.csv")
+df = spark.read.text("hdfs://namenode:9000/Datasets/train.csv")
 
-# Afficher les resultats
-df.show()
+#************************* Exercice 2 et Exercice 3 ****************
+ 
+#1/ voir les premiers lignes de donnees
+df.show(5)
+ 
+#2/voir les les types de colonnes
+print(df.printSchema())
+ 
+#3/voir le nombre de lignes
+nombre_lignes = str(df.count())
+# Afficher le resultat
+print("Nombre de lignes :" + nombre_lignes)
+ 
+#4/ compter les valeurs manquantes
+for col in df.columns:
+    missing_count = df.filter(df[col].isNull()).count()
+    print('Nombre de valeurs manquantes dans la colonne {} : {}'.format(col,missing_count))
+ 
+#5/compter les duplicas dans le dataFrame
+nb_duplicas = df.groupBy(df.columns).count().filter("count > 1").count()
+print("nombre de ligne : {}".format(nb_duplicas))
 
 # Fermeture de la session Spark
 spark.stop()
